ad0609e00fe83f75381b4cbc9e8f0639344aebbb
JoaoStipanic <joao.salmeida@sptech.school>
2026-02-19 14:14:05 -0300

    Atualizando campo da tabela usuario

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -5,12 +5,12 @@ USE stsecurity;
 CREATE TABLE empresa (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
+	cnpj CHAR(14) NOT NULL UNIQUE,
+	codigo_ativacao VARCHAR(50) NOT NULL UNIQUE 
 );
 
 CREATE TABLE usuario (
-	idPessoa INT PRIMARY KEY auto_increment,
+	idUsuario INT PRIMARY KEY auto_increment,
     fkEmpresa INT, FOREIGN KEY (fkEmpresa) REFERENCES empresa(id),
     nome VARCHAR(100),
     email VARCHAR(200),
