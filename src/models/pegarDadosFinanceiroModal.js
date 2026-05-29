1fde650290f4b657058b970202d628c662fbfb64
GabrielMarRib <gabriel.mribeiro@sptech.school>
2026-05-29 13:05:36 -0300

    fix: Fiz a maneira correta de se pegar os dados da AWS

--- src/models/pegarDadosFinanceiroModal.js
+++ src/models/pegarDadosFinanceiroModal.js
@@ -1,25 +1,36 @@
-var database = require("../database/config");
-//return database.executar(instrucaoSql);
+const database = require("../database/config");
+require("dotenv").config({ path: ".env.dev" });
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+// npm install @aws-sdk/client-s3 dotenv
 
+const s3Client = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.aws_access_key_id,
+        secretAccessKey: process.env.aws_secret_access_key,
+        sessionToken: process.env.aws_session_token
+    }
+});
 
 async function pegarDadosFinanceiro(bucket) {
     
-    const urlS3 = `https://${bucket}.s3.amazonaws.com/client/dashboard_financeiro.json`;
+    const parametros = {
+        Bucket: bucket,
+        Key: "client/dashboard_financeiro.json" 
+    };
     try {
-        console.log("Entrou no try")
-        const resposta = await fetch(urlS3);
-        if (!resposta.ok) {
-            throw new Error("Erro de rede ao acessar o S3");
-        }
-        const dados = await resposta.json();
-        console.log("Dados carregados com sucesso no Modal:", dados);
+        console.log("Entrou no try para buscar no S3 via AWS SDK");
+        
+        const command = new GetObjectCommand(parametros);
+        const resposta = await s3Client.send(command);    
+        const stringData = await resposta.Body.transformToString();
+        const dados = JSON.parse(stringData);
+        console.log("Dados carregados com sucesso do S3:", dados);
         return dados;
-
     } catch (erro) {
         console.error("Erro ao puxar dados do S3:", erro);
+        throw erro; 
     }
-
-
 }
 
 module.exports = {
